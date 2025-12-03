75d942c42cd9c1e7c2b7dc3d1a8a94735f7837fc
naofumi-fujii <naofumi.fujii@gmail.com>
2025-12-03 10:28:10 +0900

    バージョンを0.8.0に更新

--- Casks/banzai.rb
+++ Casks/banzai.rb
@@ -1,6 +1,6 @@
 cask "banzai" do
-  version "0.7.0"
-  sha256 "d0cacde044dd2caf9d8b6d6879da2a0f93572b71ed1bd09ca1f014dd81cc3112"
+  version "0.8.0"
+  sha256 "c23e892b512f396ea4180b008d111f82f80a7b603eb3ed62d9ac42ff4cdd3fe4"
 
   url "https://github.com/naofumi-fujii/banzai/releases/download/v#{version}/Banzai-v#{version}.zip"
   name "Banzai"
